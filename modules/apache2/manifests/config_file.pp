8ae00070aa301b243f97d05f5c7190ad8a99f28f
Saagar Milind Deshpande <saagar@hcs.harvard.edu>
2014-04-01 04:43:50 +0000

    fixed a lot of syntax but now there's a duplication error???

--- modules/apache2/manifests/config_file.pp
+++ modules/apache2/manifests/config_file.pp
@@ -5,82 +5,9 @@ define apache2::config_file () {
     ensure  => file,
     path    => '/etc/apache2/{$title}',
     source  => 'puppet:///modules/apache2/apache2/{$title}',
-    owner   => root
-    group   => root
+    owner   => root,
+    group   => root,
     notify  => Service['apache2'],
-    require => Package['apache2'],
+    require => Package['apache2']
   }
 }
-
-define apache2::vhost() {
-  include apache2
-
-  file {"${title}-site-availible":
-    ensure  => file,
-    path    => "/etc/apache2/sites-availible/{$title}",
-    source  => "puppet:///modules/apache2/apache2/sites-availible/{$title}",
-    owner   => root
-    group   => root
-    notify  => Service['apache2'],
-    require => Package['apache2'],
-  }
-
-  file {"${title}-site-enabled":
-    path    => "/etc/apache2/sites-enabled/{$title}",
-    ensure  => link,
-    target  => "/etc/apache2/sites-availible/{$title}",
-    owner   => root
-    group   => root
-    require => Package['apache2'],
-    require => File["${title}-site-availible"],
-  }
-}
-
-
-/* With conf specifies whether conf file should be copied as well. */
-define apache2::mod($with_conf = false) {
-  include apache2
-
-  file {"${title}-mod-availible":
-    ensure  => file,
-    path    => "/etc/apache2/mods-availible/{$title}.load",
-    source  => "puppet:///modules/apache2/apache2/mods-availible/{$title}.load",
-    owner   => root
-    group   => root
-    notify  => Service['apache2'],
-    require => Package['apache2'],
-  }
-
-  file {"${title}-mod-enabled":
-    path    => "/etc/apache2/mods-enabled/{$title}.load",
-    ensure  => link,
-    target  => "/etc/apache2/mods-availible/{$title}.load",
-    owner   => root
-    group   => root
-    notify  => Service['apache2'],
-    require => Package['apache2'],
-    require => File["${title}-mod-availible"],
-  }
-
-  if $with_conf {
-    file {"${title}-mod-availible-conf":
-      ensure  => file,
-      path    => "/etc/apache2/mods-availible/{$title}.conf",
-      source  => "puppet:///modules/apache2/apache2/mods-availible/{$title}.conf",
-      owner   => root
-      group   => root
-      require => Package['apache2'],
-    }
-
-    file {"${title}-mod-enabled-conf":
-      path    => "/etc/apache2/mods-enabled/{$title}.conf",
-      ensure  => link,
-      target  => "/etc/apache2/mods-availible/{$title}.conf",
-      owner   => root
-      group   => root
-      require => Package['apache2'],
-      require => File["${title}-mod-availible"],
-    }
-  }
-}
-
