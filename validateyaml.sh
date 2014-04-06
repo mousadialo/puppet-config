886903d74cc64a1b674fc7826997ad59692146a7
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-06 18:31:28 -0400

    yaml validator + web.yaml

---
+++ validateyaml.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# find all yaml files
+FILES=`find ./ -type f -name \*.yaml`
+# try to load the yaml file with ruby. if it fails, then we have bad yaml!
+for YAML in $FILES; do
+  echo "Validating ${YAML} ..."
+  ruby -e "require 'yaml'; YAML.load_file('${YAML}')"
+done
