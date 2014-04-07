c32ca5a116005ca4d8a9dcf08327d696d48cd0e4
Salvatore Rinchiera <salvatore@rinchiera.com>
2014-04-07 18:32:02 -0400

    first ZFS

--- modules/zfs/manifests/init.pp
+++ modules/zfs/manifests/init.pp
@@ -1,3 +1,19 @@
 class zfs {
+  include apt
 
+  apt::ppa { 'ppa:zfs-native/stable': }
+  ->
+  package { 'ubuntu-zfs':
+    ensure => latest,
+  }
+  ->
+  zpool { 'tank':
+    ensure => present
+    raidz  => ['xvdf', 'xvdg', 'xvdh', 'xvdi', 'xvdj']
+  }
+  ->
+  zfs {'tank/home':
+    ensure   => present,
+    sharenfs => on
+  }
 }
