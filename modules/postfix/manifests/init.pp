ca1a53445c466ca116ebae59aeca537f3d9a7688
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-12 22:54:28 -0400

    postfix configuration needs testing

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -1,3 +1,4 @@
+# postfix configuration for HCS mail servers
 class postfix {
 
   package { 'postfix':
@@ -9,16 +10,59 @@ class postfix {
   }
 
   service { 'postfix':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
     require => Package['postfix'],
   }
 
+  # main.cf configuration
   file { '/etc/postfix/main.cf':
-    ensure => present,
-    source => 'puppet:///modules/ntp/ntp.conf',
-    notify => Service['postfix'],
+    ensure  => present,
+    source  => 'puppet:///modules/postfix/main.cf',
+    notify  => Service['postfix'],
     require => Package['postfix']
   }
+
+  # master.cf configuration
+  file { '/etc/postfix/master.cf':
+    ensure  => present,
+    source  => 'puppet:///modules/postfix/master.cf',
+    notify  => Service['postfix'],
+    require => Package['postfix']
+  }
+
+  file { '/etc/postfix/nobl_cidr':
+    ensure  => present,
+    source  => 'puppet:///modules/postfix/nobl_cidr',
+    notify  => Service['postfix'],
+    require => Package['postfix']
+  }
+
+  # we want to ensure the following files are postmapped if modified
+  # this means that we will refresh the postmap dbs automatically
+  # the postmapfile command also adds the file to the correct location
+  postfix::postmapfile { 'access': name => access }
+  postfix::postmapfile { 'blacklist': name => blacklist }
+  postfix::postmapfile { 'canonical': name => canonical }
+  postfix::postmapfile { 'nobl': name => nobl }
+
+  # we want to ensure that we postalias the aliases files
+  exec { 'postalias_aliases':
+    command     => '/usr/sbin/postalias /etc/aliases',
+    refreshonly =>  true,
+    require     => [Package['postfix'], File["/etc/postfix/${name}"]],
+    notify      => Service['postfix']
+  }
+
+  file { '/etc/aliases':
+    ensure  => file,
+    path    => '/etc/aliases',
+    source  => 'puppet://modules/postfix/aliases',
+    owner   => 'root',
+    group   => 'root',
+    notify  => Exec['postalias_aliases'],
+    require => Package['postfix']
+  }
+
 
 }
---
+++ modules/postfix/manifests/postmapfile.pp
@@ -0,0 +1,22 @@
+# function to postmap a file if changed
+define postfix::postmapfile ($name) {
+  include postfix
+
+  exec { "postmap${name}":
+    command     => "/usr/sbin/postmap /etc/postfix/${name}",
+    refreshonly =>  true,
+    require     => [Package['postfix'], File["/etc/postfix/${name}"]],
+    notify      => Service['postfix']
+  }
+
+  file { "/etc/postfix/${name}":
+    ensure  => file,
+    path    => "/etc/postfix/${name}",
+    source  => "puppet://modules/postfix/${title}",
+    owner   => 'root',
+    group   => 'root',
+    notify  => Exec["postmap${name}"],
+    require => Package['postfix']
+  }
+
+}
