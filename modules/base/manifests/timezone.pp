732a51fb148714570b9590908a838aaf40b4a51e
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-14 19:37:05 -0400

    added base to all ubuntu machines

---
+++ modules/base/manifests/timezone.pp
@@ -0,0 +1,23 @@
+# Timezone -> America/New_York
+class base::timezone {
+
+  file { '/etc/timezone':
+    ensure  => present,
+    content => "America/New_York\n",
+  }
+
+  exec { 'reconfigure-tzdata':
+    user    => root,
+    group   => root,
+    command => '/usr/sbin/dpkg-reconfigure --frontend noninteractive tzdata',
+  }
+
+  notify { 'timezone-changed':
+    message => 'Timezone was updated to America/New_York.',
+  }
+
+  # ensure ordering
+  File['/etc/timezone'] -> Exec['reconfigure-tzdata'] -> Notify['timezone-changed']
+
+
+}
