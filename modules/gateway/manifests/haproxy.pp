02b9fa337f068208c14d7cb52dde001479fb9381
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-14 22:34:47 -0400

    haproxy stuff

--- modules/gateway/manifests/haproxy.pp
+++ modules/gateway/manifests/haproxy.pp
@@ -1,6 +1,23 @@
 # configuration for haproxy
 class gateway::haproxy {
 
+  package { 'haproxy':
+    ensure => installed
+  }
+
+  service { 'haproxy':
+    ensure  => running,
+    enable  => true,
+    require => Package['haproxy'],
+  }
+
+  # haproxy user, doesn't need a real shell
+  user { 'haproxy':
+    name    => 'haproxy',
+    home    => '/var/www',
+    shell   => '/bin/false',
+    require => Package['haproxy']
+  }
 
 
 }
