a66c39a72abe4af5b9ce3f080c2ad56a587f8b91
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-16 23:02:42 -0400

    ldap host

--- validateyaml.sh
+++ validateyaml.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Validating YAML files. Should not print an error if valid."
 # find all yaml files
 FILES=`find ./ -type f -name \*.yaml`
 # try to load the yaml file with ruby. if it fails, then we have bad yaml!
