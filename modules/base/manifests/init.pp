35bd1567d1b2da97cf594d07f7324d88d3544a42
Saagar Deshpande <sdeshpande@college.harvard.edu>
2014-04-24 02:19:32 -0400

    ubuntu base packages

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -3,11 +3,16 @@ class base {
   include base::timezone
   include base::users
 
-  # install the base packages listed in data/Ubuntu.yaml
-  /*/$packages = hiera_array('base_packages')
+  # install the base packages listed in data/hcs.yaml
+  /*$packages = hiera_array('userfacing_packages')
   package { $packages:
     ensure => installed
   }
   */
+  # install the base packages listed in data/Ubuntu.yaml
+  #$packages_ubuntu = hiera_array('base_packages')
+  #package { $packages:
+    #ensure => installed
+  #}
 
 }
