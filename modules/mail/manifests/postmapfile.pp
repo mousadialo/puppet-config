3f02f5c3b37a01c9c2ee194a69a4fd3d87966fd8
Salvatore Rinchiera <salvatore@rinchiera.com>
2014-04-25 08:15:05 +0000

    postmap works

--- modules/mail/manifests/postmapfile.pp
+++ modules/mail/manifests/postmapfile.pp
@@ -2,23 +2,35 @@
 define mail::postmapfile ($name, $map=false) {
   include mail::postfix
 
-  file { "/etc/postfix/${name}":
-    ensure  => file,
-    path    => "/etc/postfix/${name}",
-    source  => "puppet:///modules/mail/postfix/${title}",
-    owner   => 'root',
-    group   => 'root',
-    require => Package['postfix']
-  }
 
-  # we don't want to postmap everything, because some files are config files
-  if $map == true {
+  if $map {
+
+    file { "/etc/postfix/${name}":
+      ensure  => file,
+      path    => "/etc/postfix/${name}",
+      source  => "puppet:///modules/mail/postfix/${title}",
+      owner   => 'root',
+      group   => 'root',
+      notify  => Exec["postmap${name}"],
+      require => Package['postfix']
+    }
+
     exec { "postmap${name}":
       command     => "/usr/sbin/postmap /etc/postfix/${name}",
-      refreshonly =>  true,
       require     => [Package['postfix'], File["/etc/postfix/${name}"]],
       notify      => Service['postfix']
     }
+
+  } else {
+
+    file { "/etc/postfix/${name}":
+      ensure  => file,
+      path    => "/etc/postfix/${name}",
+      source  => "puppet:///modules/mail/postfix/${title}",
+      owner   => 'root',
+      group   => 'root',
+      require => Package['postfix']
+    }
+
   }
-  notify {"map is ${map}"}
 }
