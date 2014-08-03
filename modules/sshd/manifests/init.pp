fea1dd30b66b5622e3f693e2e1431ccbe7aded58
Salvatore Rinchiera <salvatore@rinchiera.com>
2014-08-02 20:53:26 -0400

    Cant login through filers with ldap but you can use ldap to lookup uid and gid

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -15,6 +15,16 @@ class sshd {
       mode   => '0644',
       notify => Service['ssh']
     }
+  } else if $::machine_type == 'file' {
+    # Web config is a little different in order to make helios work
+    file {'/etc/ssh/sshd_config':
+      ensure => file,
+      source => 'puppet:///modules/sshd/sshd_config.file',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      notify => Service['ssh']
+    }
   } else {
     file {'/etc/ssh/sshd_config':
       ensure => file,
