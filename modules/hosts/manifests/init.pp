825532bf2e535327ac6d9e538bcc5512b4188d2b
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-06-28 03:18:56 -0700

    Update hosts module.

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -42,4 +42,21 @@ class hosts {
     ip     => '127.0.0.1',
   }
   
+  # Workaround to delay puppet from evaluating the actual host resource.
+  # More info here: https://groups.google.com/forum/#!topic/puppet-users/Ux28jzVpteU
+  define exported_host ($ensure, $ip) {
+    host { $title:
+      ensure => $ensure,
+      ip     => $ip,
+    }
+  }
+  # Export host resource for other machines to use.
+  # More on exported resources: https://docs.puppetlabs.com/puppet/latest/reference/lang_exported.html
+  @@exported_host { $::fqdn:
+    ensure => present,
+    ip     => $::ipaddress,
+  }
+  # Collect other machines' exported resources.
+  Exported_host <<| title != $::fqdn |>>
+  
 }
