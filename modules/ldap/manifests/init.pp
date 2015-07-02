367dbd07cd7cc67900e7e0a69dd331af7216701c
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-02 10:56:31 -0700

    Auto manage ldap servers using concat fragments.

--- modules/ldap/manifests/init.pp
+++ modules/ldap/manifests/init.pp
@@ -2,30 +2,46 @@
 # includes config for HCS LDAP server and client config for all other servers
 class ldap {
   
+  $ldap_conf = '/etc/ldap/ldap.conf'
+  
   package { 'ldap-utils':
     ensure => installed,
   }
 
-  if $::machine_type == 'ldap' {
-    include ldap::server
-    
-    exported_vars::set { 'ldap_server':
-        value => "ldap://${::fqdn}"
-    }
-  }
-  else {
-    include ldap::client    
-  }
-  
-  $ldap_servers = get_exported_var('', 'ldap_server', ["test"])
-  
-  file { '/etc/ldap/ldap.conf':
+  concat { $ldap_conf:
     ensure  => file,
     content => template('ldap/ldap.conf.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     require => Package['ldap-utils'],
+  }
+  
+  concat::fragment { 'ldap_conf_head':
+    target => $ldap_conf,
+    source => 'puppet:///modules/ldap/ldap.conf.head',
+    order  => '1',
+  }
+  
+  if $::machine_type == 'ldap' {
+    include ldap::server
+    
+    @@concat::fragment { 'ldap_conf_${::hostname}':
+      target => $ldap_conf,
+      source => "ldap://${::fqdn}\t",
+      order  => '2',
+    }
+  }
+  else {
+    include ldap::client    
+  }
+  
+  Concat::Fragment <<| target == $ldap_conf |>>
+  
+  concat::fragment { 'ldap_conf_tail':
+    target => $ldap_conf,
+    source => 'puppet:///modules/ldap/ldap.conf.tail',
+    order  => '3',
   }
 
 }
