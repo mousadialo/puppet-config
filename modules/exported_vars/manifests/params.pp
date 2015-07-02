98ffd7652003f7156e22667b2e6eee16d3fddf1c
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-02 11:06:47 -0700

    Removed exported_vars module.

--- modules/exported_vars/manifests/params.pp
+++
@@ -1,7 +0,0 @@
-# Class: exported_vars::params
-#
-# This class sets the default variables used by exported_vars
-#
-class exported_vars::params {
-  $basedir ='/var/lib/puppet/exported_var'
-}
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -2,8 +2,6 @@
 class puppet {
 
   if $::machine_type == 'puppetmaster' {
-    include exported_vars
-  
     # More on hiera-eyaml: https://github.com/TomPoulton/hiera-eyaml
     # Ensure that the public and private .pem files are placed in /etc/puppet/secure/keys.
     package { 'hiera-eyaml':
