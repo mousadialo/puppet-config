c9e50862aa0306b5f9b9709ea2d26caa457dffa5
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-13 12:48:26 -0700

    Manage ldap.conf using file_line resources.

--- modules/ldap/manifests/init.pp
+++ modules/ldap/manifests/init.pp
@@ -2,46 +2,28 @@
 # includes config for HCS LDAP server and client config for all other servers
 class ldap {
   
-  $ldap_conf = '/etc/ldap/ldap.conf'
-  
   package { 'ldap-utils':
     ensure => installed,
-  }
-
-  concat { $ldap_conf:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    warn    => true,
-    require => Package['ldap-utils'],
-  }
-  
-  concat::fragment { 'ldap_conf_head':
-    target => $ldap_conf,
-    source => 'puppet:///modules/ldap/ldap.conf.head',
-    order  => '1',
   }
   
   if $::machine_type == 'ldap' {
     include ldap::server
-    
-    @@concat::fragment { "ldap_conf_${::hostname}":
-      target  => $ldap_conf,
-      content => "ldap://${::fqdn}\t",
-      order   => '2',
-    }
   }
   else {
     include ldap::client
   }
-  
-  Concat::Fragment <<| target == $ldap_conf |>>
-  
-  concat::fragment { 'ldap_conf_tail':
-    target => $ldap_conf,
-    source => 'puppet:///modules/ldap/ldap.conf.tail',
-    order  => '3',
+
+  file_line { 'ldap_base':
+    path  => '/etc/ldap/ldap.conf',
+    line  => "BASE\tdc=hcs,dc=harvard,dc=edu",
+    match => '^#?BASE\s+\S+$',
+  }
+
+  $ldap_server = hiera('ldap-server')
+  file_line { 'ldap_uri':
+    path  => '/etc/ldap/ldap.conf',
+    line  => "URI\tldap://${ldap_server}",
+    match => '^#?URI\s+\S+',
   }
 
 }
