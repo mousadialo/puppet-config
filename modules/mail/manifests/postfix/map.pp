eada2e9df0b9afa26e66f5194b04564748cd6f7b
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-12 17:27:16 -0700

    Updated mail module.

--- modules/mail/manifests/postfix/map.pp
+++ modules/mail/manifests/postfix/map.pp
@@ -3,8 +3,9 @@ define mail::postfix::map () {
 
   mail::postfix::config { $title: } ~>
   exec { "/usr/sbin/postmap cdb:/etc/postfix/${title}":
-    require => [Package['postfix'], Package['postfix-cdb']],
-    notify  => Service['postfix'],
+    refreshonly => true,
+    require     => [Package['postfix'], Package['postfix-cdb']],
+    notify      => Service['postfix'],
   }
 
 }
