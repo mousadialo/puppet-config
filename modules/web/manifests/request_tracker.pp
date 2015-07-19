3f19447c8a8f5b14aa45756734d861a4a12ea015
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-19 02:13:36 -0700

    Update rt configs.

--- modules/web/manifests/request_tracker.pp
+++ modules/web/manifests/request_tracker.pp
@@ -15,33 +15,61 @@ class web::request_tracker {
       require => Package['request-tracker4'],
     }
     
-    #file { '/etc/request-tracker4/RT_SiteConfig.d':
-    #  ensure  => directory,
-    #  recurse => true,
-    #  purge   => true,
-    #  source  => 'puppet:///modules/web/request-tracker4/RT_SiteConfig.d',
-    #  owner   => 'root',
-    #  group   => 'root',
-    #  mode    => '0600',
-    #  require => Package['request-tracker4'],
-    #  notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
-    #}
+    file { '/etc/request-tracker4/RT_SiteConfig.d/40-timezone:
+      ensure  => file,
+      source  => 'puppet:///modules/web/request-tracker4/RT_SiteConfig.d/40-timezone',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['request-tracker4'],
+      notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
+    }
     
-    #$rt_database_password = hiera('rt-database-password')
-    #file { '/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig':
-    #  ensure  => file,
-    #  content => template('web/request-tracker4/RT_SiteConfig.d/51-dbconfig.erb'),
-    #  owner   => 'root',
-    #  group   => 'root',
-    #  mode    => '0600',
-    #  require => Package['request-tracker4'],
-    #  notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
-    #}
+    file { '/etc/request-tracker4/RT_SiteConfig.d/50-debconf:
+      ensure  => file,
+      source  => 'puppet:///modules/web/request-tracker4/RT_SiteConfig.d/50-debconf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      require => Package['request-tracker4'],
+      notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
+    }
     
-    #exec { '/usr/sbin/update-rt-siteconfig-4':
-    #  refreshonly => true,
-    #  notify      => Service['apache2'],
-    #}
+    $rt_database_password = hiera('rt-database-password')
+    file { '/etc/request-tracker4/RT_SiteConfig.d/51-dbconfig-common':
+      ensure  => file,
+      content => template('web/request-tracker4/RT_SiteConfig.d/51-dbconfig-common.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      require => Package['request-tracker4'],
+      notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
+    }
+    
+    file { '/etc/request-tracker4/RT_SiteConfig.d/52-ldap:
+      ensure  => file,
+      source  => 'puppet:///modules/web/request-tracker4/RT_SiteConfig.d/52-ldap',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      require => Package['request-tracker4'],
+      notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
+    }
+    
+    file { '/etc/request-tracker4/RT_SiteConfig.d/99-other:
+      ensure  => file,
+      source  => 'puppet:///modules/web/request-tracker4/RT_SiteConfig.d/99-other',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['request-tracker4'],
+      notify  => Exec['/usr/sbin/update-rt-siteconfig-4'],
+    }
+    
+    exec { '/usr/sbin/update-rt-siteconfig-4':
+      refreshonly => true,
+      notify      => Service['apache2'],
+    }
     
     exec { '/usr/bin/cpan -i RT::Authen::ExternalAuth':
       environment => 'PERL_MM_USE_DEFAULT=1',
