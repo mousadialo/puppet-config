1dca91f33ea4281d33e408bba40f0e8c76a1e96e
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-07-18 17:10:18 -0700

    Update modsecurity configuration.

--- modules/web/manifests/modsecurity/rule.pp
+++ modules/web/manifests/modsecurity/rule.pp
@@ -1,12 +1,12 @@
-define web::modsecurity::rule ($rule_type) {
+define web::modsecurity::rule ($type) {
 
-  validate_re($rule_type, '^(experimental|optional|slr)$',
+  validate_re($type, '^(experimental|optional|slr)$',
     "${rule_type} is not supported for rule_type.
     Allowed values are 'experimental', 'optional', and 'slr'.")
 
   file { "/usr/share/modsecurity-crs/activated_rules/${title}":
     ensure  => link,
-    target  => "/usr/share/modsecurity-crs/${rule_type}_rules/${title}",
+    target  => "/usr/share/modsecurity-crs/${type}_rules/${title}",
     owner   => 'root',
     group   => 'root',
     require => Package['modsecurity-crs'],
