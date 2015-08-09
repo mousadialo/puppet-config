e2305cc847391655cfee6d0fd3b8b9d4bcc98cdc
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-08-08 23:58:20 -0700

    Add awscli module.

--- modules/awscli/manifests/init.pp
+++ modules/awscli/manifests/init.pp
@@ -1,6 +1,9 @@
 # Installs and configures the AWS CLI
 class awscli {
 
+  package { 'python-pip':
+    ensure => installed,
+  } ->
   package { 'awscli':
     ensure   => installed,
     provider => pip,
