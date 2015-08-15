ea53b4bd7153eba9e32cee49dfbd533472bfe84f
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-08-14 19:12:15 -0700

    Allow login servers to use LDAPS.

--- modules/ldap/manifests/init.pp
+++ modules/ldap/manifests/init.pp
@@ -21,18 +21,23 @@ class ldap {
 
   $ldap_server = hiera('ldap-server')
   if $::machine_type == 'login' {
+    # LDAPS is required to change an LDAP account password
+    # Hence, we enabled LDAPS only on login servers
     file_line { 'ldap_uri':
       path  => '/etc/ldap/ldap.conf',
       line  => "URI\tldaps://${ldap_server}",
       match => '^#?URI\s+\S+',
     }
     
+    # No need to verify certificate
     file_line { 'ldap_tls_reqcert':
       path  => '/etc/ldap/ldap.conf',
       line  => "TLS_REQCERT\tnever",
     }
   }
   else {
+    # LDAPS is not required (and may even break things like Amavis) on other servers
+    # since we are in a private network.
     file_line { 'ldap_uri':
       path  => '/etc/ldap/ldap.conf',
       line  => "URI\tldap://${ldap_server}",
