237978b459205b32994e5b77e4a6314e93674d19
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-08-18 22:27:05 -0700

    Revert "Split lists server."
    This reverts commit 1c6feb00afe6b6c7fd340edb9bd4b82570a7319e.

--- modules/filesystem/manifests/init.pp
+++ modules/filesystem/manifests/init.pp
@@ -64,7 +64,7 @@ class filesystem {
     }
   }
   
-  if $::machine_type == 'mailman' {
+  if $::machine_type == 'lists' {
     # Mount mailman directory
     nfs::client::mount { 'mailman':
       server  => $nfs_server,
