1c6feb00afe6b6c7fd340edb9bd4b82570a7319e
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-08-18 21:22:17 -0700

    Split lists server.

--- modules/filesystem/manifests/init.pp
+++ modules/filesystem/manifests/init.pp
@@ -64,7 +64,7 @@ class filesystem {
     }
   }
   
-  if $::machine_type == 'lists' {
+  if $::machine_type == 'mailman' {
     # Mount mailman directory
     nfs::client::mount { 'mailman':
       server  => $nfs_server,
