a9b2040b84defcc0f60c51449405b2c4b36c1a3f
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-08-20 13:18:52 -0700

    Update apache2 mod test commands.

--- modules/web/manifests/apache2/mod.pp
+++ modules/web/manifests/apache2/mod.pp
@@ -7,14 +7,14 @@ define web::apache2::mod ($ensure = 'enabled') {
   
   if $ensure == 'enabled' {
     exec { "/usr/sbin/a2enmod ${title}" :
-      unless  => "(/bin/readlink -e /etc/apache2/mods-enabled/${title}.load && (! /usr/bin/test -f /etc/apache2/mods-available/${title}.conf || /bin/readlink -e /etc/apache2/mods-enabled/${title}.conf)) 1> /dev/null",
+      unless  => "/bin/echo '(/bin/readlink -e /etc/apache2/mods-enabled/${title}.load && (! /usr/bin/test -f /etc/apache2/mods-available/${title}.conf || /bin/readlink -e /etc/apache2/mods-enabled/${title}.conf))' | /bin/bash 1> /dev/null",
       notify  => Service['apache2'],
       require => Package['apache2'],
     }
   }
   else {
     exec { "/usr/sbin/a2dismod ${title}" :
-      onlyif  => "(/bin/readlink -e /etc/apache2/mods-enabled/${title}.load || /bin/readlink -e /etc/apache2/mods-enabled/${title}.conf) 1> /dev/null",
+      onlyif  => "/bin/echo '(/bin/readlink -e /etc/apache2/mods-enabled/${title}.load || /bin/readlink -e /etc/apache2/mods-enabled/${title}.conf)' | /bin/bash 1> /dev/null",
       notify  => Service['apache2'],
       require => Package['apache2'],
     }
