cb4b9e9f209dafd346e1a93f528a87df469346ea
Frederick Widjaja <frederick_widjaja@hotmail.com>
2015-12-31 21:13:23 +0700

    Add skel dir files.

---
+++ modules/hcsscripts/files/acctutils/data/hcs.bashrc
@@ -0,0 +1,4 @@
+# HCS Global bashrc File
+
+# possibility of multiple login machines
+export PS1="${debian_chroot:+($debian_chroot)}\u@hcs:\w\$ "
--- modules/hcsscripts/manifests/acctutils.pp
+++ modules/hcsscripts/manifests/acctutils.pp
@@ -19,6 +19,31 @@ class hcsscripts::acctutils {
       group   => 'root',
     }
     
+    file { '/etc/skel':
+      ensure  => directory,
+      recurse => true,
+      purge   => true,
+      source  => 'puppet:///modules/hcsscripts/acctutils/data/skel',
+      owner   => 'root',
+      group   => 'root',
+      # To prevent these files from appearing in the hcs home dir
+      require => User['hcs'],
+    }
+    
+    file { '/etc/hcs.bashrc':
+      ensure  => file,
+      source  => 'puppet:///modules/hcsscripts/acctutils/data/hcs.bashrc',
+      owner   => 'root',
+      group   => 'root',
+    }
+    
+    file { '/etc/hcs.tcshrc':
+      ensure  => file,
+      source  => 'puppet:///modules/hcsscripts/acctutils/data/hcs.tcshrc',
+      owner   => 'root',
+      group   => 'root',
+    }
+    
     file { '/etc/hcs/mail_templates':
       ensure  => directory,
       recurse => remote,
