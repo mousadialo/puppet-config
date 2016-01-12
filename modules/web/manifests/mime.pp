d2beea0b1daca4ae5eed3263bcf9dd929a0e74cd
Frederick Widjaja <frederick_widjaja@hotmail.com>
2016-01-12 09:08:34 +0700

    Mime configuration.

--- modules/web/manifests/mime.pp
+++ modules/web/manifests/mime.pp
@@ -5,42 +5,42 @@ class web::mime {
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php			phtml pht php',
     match => '^#?application/x-httpd-php',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
   
   file_line { 'x-httpd-php-source':
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php-source			phps',
     match => '^#?application/x-httpd-php-source',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
   
   file_line { 'x-httpd-php3':
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php3			php3',
     match => '^#?application/x-httpd-php3',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
   
   file_line { 'x-httpd-php3-preprocessed':
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php3-preprocessed		php3p',
     match => '^#?application/x-httpd-php3-preprocessed',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
   
   file_line { 'x-httpd-php4':
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php4			php4',
     match => '^#?application/x-httpd-php4',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
   
   file_line { 'x-httpd-php5':
     path  => '/etc/mime.types',
     line  => 'application/x-httpd-php5			php5',
     match => '^#?application/x-httpd-php5',
-    notify  => Service['apache'],
+    notify  => Service['apache2'],
   }
 
 }
